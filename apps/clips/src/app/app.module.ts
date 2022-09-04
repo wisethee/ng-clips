0aef50646757e88ae676490a7afd2d08d4f2f455
marius <marius.paduraru@gmail.com>
2022-09-04 09:08:21 +0100

    feat: add nav and auth-modal components and move corresponding tags from app

--- apps/clips/src/app/app.module.ts
+++ apps/clips/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NavComponent],
+  imports: [BrowserModule, UserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/clips/src/app/user/auth-modal/auth-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-clips-auth-modal',
+  templateUrl: './auth-modal.component.html',
+  styleUrls: ['./auth-modal.component.scss'],
+})
+export class AuthModalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
