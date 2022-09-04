f0d7f0330e45c5b77b8fb1bc7bec81a79fef35d8
marius <marius.paduraru@gmail.com>
2022-09-04 09:12:16 +0100

    feat: create a shared module, add modal component to shared module

---
+++ apps/clips/src/app/shared/modal/modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-clips-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+})
+export class ModalComponent {}
