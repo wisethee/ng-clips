736368179ef0a92d51ec84805789b43bcc5b727b
marius <marius.paduraru@gmail.com>
2022-09-05 10:30:45 +0100

    build: add firebase

--- apps/clips/src/app/app.module.ts
+++ apps/clips/src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [AppComponent, NavComponent],
-  imports: [BrowserModule, UserModule],
+  imports: [
+    BrowserModule,
+    UserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/clips/src/environments/environment.prod.ts
+++ apps/clips/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyAEgsMibAmiOCGSzMBCZY2-YTtS80BUMfk',
+    authDomain: 'ng-clips-6668b.firebaseapp.com',
+    projectId: 'ng-clips-6668b',
+    storageBucket: 'ng-clips-6668b.appspot.com',
+    messagingSenderId: '397870136718',
+    appId: '1:397870136718:web:dffe7dda3ad89d519838df',
+  },
 };
