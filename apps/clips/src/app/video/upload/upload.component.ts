5b4b828f51af4b64de970e0f343ca4d8c8179699
marius <marius.paduraru@gmail.com>
2022-09-06 10:38:54 +0100

    feat: add fallback uploads functionality

--- apps/clips/src/app/video/upload/upload.component.ts
+++ apps/clips/src/app/video/upload/upload.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { last } from 'rxjs';
+import { last, switchMap } from 'rxjs';
 import { v4 as uuid } from 'uuid';
+import firebase from 'firebase/compat';
+import { ClipService } from '../../services/clip.service';
 
 @Component({
   selector: 'ng-clips-upload',
@@ -22,6 +25,8 @@ export class UploadComponent {
   showPercentage = false;
   percentage = 0;
 
+  user: firebase.User | null = null;
+
   title = new FormControl('', {
     validators: [Validators.required, Validators.minLength(3)],
     nonNullable: true,
@@ -31,12 +36,20 @@ export class UploadComponent {
     title: this.title,
   });
 
-  constructor(private storage: AngularFireStorage) {}
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth,
+    private clipService: ClipService
+  ) {
+    auth.user.subscribe((user) => (this.user = user));
+  }
 
   storeFile(event: Event) {
     this.isDragover = false;
 
-    this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    this.file = (event as DragEvent).dataTransfer
+      ? (event as DragEvent).dataTransfer?.files.item(0) ?? null
+      : (event.target as HTMLInputElement).files?.item(0) ?? null;
 
     if (!this.file || this.file.type !== 'video/mp4') {
       return;
@@ -48,6 +61,8 @@ export class UploadComponent {
   }
 
   uploadFile() {
+    this.uploadForm.disable();
+
     this.showAlert = true;
     this.alertColor = 'blue';
     this.alertMessage = 'Please wait! Your clip is being uploaded.';
@@ -58,20 +73,36 @@ export class UploadComponent {
     const clipPath = `clips/${clipFileName}.mp4`;
 
     const task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
+
     task
       .percentageChanges()
       .subscribe((progress) => (this.percentage = (progress as number) / 100));
 
     task
       .snapshotChanges()
-      .pipe(last())
+      .pipe(
+        last(),
+        switchMap(() => clipRef.getDownloadURL())
+      )
       .subscribe({
-        next: () => {
+        next: (url) => {
+          const clip = {
+            uid: this.user?.uid as string,
+            displayName: this.user?.displayName as string,
+            title: this.title.value,
+            fileName: `${clipFileName}.mp4`,
+            url,
+          };
+
+          this.clipService.createClip(clip);
+
           this.alertColor = 'green';
           this.alertMessage = 'Success!, Your clip is uploaded.';
           this.showPercentage = false;
         },
         error: (error) => {
+          this.uploadForm.enable();
           this.alertColor = 'red';
           this.alertMessage = 'Upload failed! Please try again later.';
           this.inSubmission = true;
