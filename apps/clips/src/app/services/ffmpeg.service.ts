1c91fff59850749fc40edd94af5146f46d2a14cd
marius <marius.paduraru@gmail.com>
2022-09-06 17:50:49 +0100

    feat: generate screenshots

--- apps/clips/src/app/services/ffmpeg.service.ts
+++ apps/clips/src/app/services/ffmpeg.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { createFFmpeg } from '@ffmpeg/ffmpeg';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
 @Injectable({
   providedIn: 'root',
@@ -20,5 +20,29 @@ export class FfmpegService {
 
     await this.ffmpeg.load();
     this.isReady = true;
+  }
+
+  async getScreenshots(file: File) {
+    const data = await fetchFile(file);
+    this.ffmpeg.FS('writeFile', file.name, data);
+
+    const seconds = [3, 6, 9];
+    const commands: string[] = [];
+
+    seconds.forEach((second, index) => {
+      commands.push(
+        '-i',
+        file.name,
+        '-ss',
+        `00:00:${second}`,
+        '-frames:v',
+        '1',
+        '-filter:v',
+        'scale=540:-1',
+        `output_0${index + 1}.png`
+      );
+    });
+
+    await this.ffmpeg.run(...commands);
   }
 }
--- apps/clips/src/app/video/upload/upload.component.ts
+++ apps/clips/src/app/video/upload/upload.component.ts
@@ -58,7 +58,7 @@ export class UploadComponent implements OnDestroy {
     this.task?.cancel();
   }
 
-  storeFile(event: Event) {
+  async storeFile(event: Event) {
     this.isDragover = false;
 
     this.file = (event as DragEvent).dataTransfer
@@ -68,6 +68,8 @@ export class UploadComponent implements OnDestroy {
     if (!this.file || this.file.type !== 'video/mp4') {
       return;
     }
+
+    await this.ffmpegService.getScreenshots(this.file);
 
     this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
 
